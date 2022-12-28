73bfea770e7ab0fcbb317e65044b3f55b1322e0d
psanvicente21 <psanvicente1986@gmail.com>
2022-12-28 09:39:49 +0100

    Agregada propiedad Visible para los atributos y comentarios

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -14,7 +14,12 @@ class Client extends Model
         'password',
         'state_id',
     ];
-    protected $hidden = ['password'];
+    //Atributos ocultos en la serialización
+    //protected $hidden = ['password'];
+
+    //Atributos visibles en la serialización
+    protected $visible = ['name','cif','state_id'];
+    //Un cliente tiene un state
     public function state(){
         return $this->belongsTo(State::class);
     }
